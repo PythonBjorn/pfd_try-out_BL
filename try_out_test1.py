6285cf5decc912bf0133f8b785e2625568a48d57
bjornlecis <bjorn.lecis@gmail.com>
2024-09-25 19:48:11 +0200

    Aanpassing 2

--- try_out_test1.py
+++ try_out_test1.py
@@ -1,5 +1,7 @@
 print("deze code komt van github, test")
 print("import gelukt")
 #code vanuit pycharm
-#commint
+#commit
 print("Deze code komt van pycharm")
+#commit sessie 2
+print("deze code komt van pycharm voor de 2e keer")
